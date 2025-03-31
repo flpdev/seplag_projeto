abe9a1f228f99ab649471a3c1c34d3f7a939139c
Felipe1~ <felipe.oliveira@mt.senac.br>
2025-03-31 15:30:02 -0400

    Impelentação autenticação API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+            HandleCors::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+    if (!$token = auth('api')->attempt($credentials)) {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+    return response()->json(['token' => $token]);
+});
+
+Route::post('/refresh', function () {
+    return response()->json(['token' => auth('api')->refresh()]);
+});
+
+Route::middleware('auth:api')->get('/me', function (Request $request) {
+    return response()->json(auth('api')->user());
+});
